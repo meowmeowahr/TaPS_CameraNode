7a0230b3fc0714e0c99f8ad073fa3dd9382d7b1d
meowmeowahr <meowmeowahr@gmail.com>
2026-08-04 16:47:26 -0500

    double frames for test

--- main.cpp
+++ main.cpp
@@ -197,6 +197,12 @@ int main(const int argc, char *argv[]) {
             }
         }
 
+        if (VideoRecordThread::getState() != VideoRecordThread::RecorderState::Saving) {
+            if (!frameBuffer.tryPush(TimestampedFrame{frame, std::chrono::system_clock::now().time_since_epoch()})) {
+                spdlog::warn("record buffer full, dropping frame #{}", frame_count);
+            }
+        }
+
         if (!frameBufferStream.tryPush(TimestampedFrame{frame, std::chrono::system_clock::now().time_since_epoch()})) {
             spdlog::warn("stream buffer full, dropping frame #{}", frame_count);
         }
