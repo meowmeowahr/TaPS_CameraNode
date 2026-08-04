8f83a8032e23646f569f991725ee818c58b47f7e
meowmeowahr <meowmeowahr@gmail.com>
2026-08-03 23:11:26 -0500

    fancy web ui from gemini

--- video_recorder.h
+++ video_recorder.h
@@ -293,7 +293,7 @@ private:
 
         for (unsigned i = 0; i < numEncoders; ++i) {
             session->encoders.emplace_back([s = session.get(), i, encoderType]() {
-                pthread_setname_np(pthread_self(), std::format("encode-{}", i).c_str());
+                pthread_setname_np(pthread_self(), std::format("encode_{}", i).c_str());
                 const std::vector params = {cv::IMWRITE_JPEG_QUALITY, s->jpegQuality};
                 while (const auto job = s->inboxes[i].pop()) {
                     Result r;
